a3fd148938d2f58b2bfeb508a249c84a0f8d727c
ProValdi <ProValdi@users.noreply.github.com>
2021-12-07 12:30:38 +0000

    Apply automatic changes

--- server-side/swagger_server/models/__init__.py
+++ server-side/swagger_server/models/__init__.py
@@ -3,5 +3,7 @@
 # flake8: noqa
 from __future__ import absolute_import
 # import models into model package
+from swagger_server.models.answer import Answer
 from swagger_server.models.exchange_payload import ExchangePayload
+from swagger_server.models.init import Init
 from swagger_server.models.table import Table
