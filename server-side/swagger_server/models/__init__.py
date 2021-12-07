26f70066fe66d66ae8244514e52c88c2b148fd48
ProValdi <ProValdi@users.noreply.github.com>
2021-12-07 16:50:13 +0000

    Apply automatic changes

--- server-side/swagger_server/models/__init__.py
+++ server-side/swagger_server/models/__init__.py
@@ -6,4 +6,5 @@ from __future__ import absolute_import
 from swagger_server.models.answer import Answer
 from swagger_server.models.exchange_payload import ExchangePayload
 from swagger_server.models.init import Init
+from swagger_server.models.inline_response200 import InlineResponse200
 from swagger_server.models.table import Table
