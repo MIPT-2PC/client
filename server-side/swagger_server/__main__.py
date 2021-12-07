e9a5c697ca8644b395c918e8bd51b49f3edd6122
ProValdi <ProValdi@users.noreply.github.com>
2021-12-07 20:02:23 +0000

    Apply automatic changes

--- server-side/swagger_server/__main__.py
+++ server-side/swagger_server/__main__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import connexion
-import os
 
 from swagger_server import encoder
 
@@ -10,11 +9,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'User exchange API'}, pythonic_params=True)
-    if os.getenv('CLIENT_A', None) is not None:
-        cliPort = os.getenv('CLIENT_A', None)
-    if os.getenv('CLIENT_B', None) is not None:
-        cliPort = os.getenv('CLIENT_B', None)
-    app.run(port=int(cliPort))
+    app.run(port=8080)
 
 
 if __name__ == '__main__':
